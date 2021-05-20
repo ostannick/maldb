2eb69a1d309786c8a9bd46b5968c16ef5ec09a2c
Nick Ostan <42483844+ostannick@users.noreply.github.com>
2021-05-20 13:31:39 -0400

    Update AnalysisController.php

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -51,42 +51,43 @@ class AnalysisController extends Controller
         //Get the table name and parent ID off the first hit.
         $table      = $data->first()['source'];
         $parent_id  = $data->first()['parent'];
+        $parent_name= $data['parent_name'];
         $observed   = $data->pluck('id')->filter();
 
         $mass_filter_lower = 650;
         $mass_filter_upper = 4000;
 
         //Get the MSO = 0, MC = 0 peptides. Let's start with these and modify as appropriate.
-        $all_peptides_stringent = \DB::table($table)
-                                        ->where([
+        \DB::table($table)
+            ->where([
 
-                                            ['parent', '=', $parent_id],
-                                            ['missed_cleavages', '=', 0],
-                                            ['met_ox_count', '=', 0]
+                ['parent', '=', $parent_id],
+                ['missed_cleavages', '=', 0],
+                ['met_ox_count', '=', 0]
 
-                                        ])
-                                        ->get()
-                                        //Mark each peptide as observed or not
-                                        ->map(function ($item) use (&$observed) {
-                                            $observed->contains($item->id) ? $item->observed = true : $item->observed = false;
-                                            return $item;
-                                        })
-                                        //Remove peptides we would not see anyways given our silly detector.
-                                        ->filter(function ($item) use (&$mass_filter_lower, &$mass_filter_upper){
-                                            return ($item->mz1_monoisotopic >= $mass_filter_lower && $item->mz1_monoisotopic <= $mass_filter_upper);
-                                        })
-                                        //I should include a function in here that filters out common contaminants like trypsin autolysis products
-                                        //This will increase the quality of the training set.
+            ])
+            ->get()
+            //Mark each peptide as observed or not
+            ->map(function ($item) use (&$observed) {
+                $observed->contains($item->id) ? $item->observed = true : $item->observed = false;
+                return $item;
+            })
+            //Remove peptides we would not see anyways given our silly detector.
+            ->filter(function ($item) use (&$mass_filter_lower, &$mass_filter_upper){
+                return ($item->mz1_monoisotopic >= $mass_filter_lower && $item->mz1_monoisotopic <= $mass_filter_upper);
+            })
+            //I should include a function in here that filters out common contaminants like trypsin autolysis products
+            //This will increase the quality of the training set.
 
-                                        //Add to the database
-                                        ->each(function ($item){
-                                            Ionizer::create([
-                                                'user_id' => Auth::user()->id,
-                                                'parent_name' => 'placeHolder',
-                                                'sequence' => $item->sequence,
-                                                'ionized' => $item->observed
-                                            ])->save();
-                                        });
+            //Add to the database
+            ->each(function ($item) use (&$parent_name){
+                Ionizer::create([
+                    'user_id' => Auth::user()->id,
+                    'parent_name' => $parent_name,
+                    'sequence' => $item->sequence,
+                    'ionized' => $item->observed
+                ])->save();
+            });
 
         return 'success';
     }
